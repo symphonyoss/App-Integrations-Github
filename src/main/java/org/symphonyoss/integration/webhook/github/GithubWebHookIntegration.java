f8b8c01e24ed10591a67cebaf0549f9d96819806
campidelli <campidelli@gmail.com>
2017-04-19 18:24:04 -0300

    APP-633: Returning HTTP 415 when the payload content-type is not supported.

--- src/main/java/org/symphonyoss/integration/webhook/github/GithubWebHookIntegration.java
+++ src/main/java/org/symphonyoss/integration/webhook/github/GithubWebHookIntegration.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016-2017 Symphony Integrations - Symphony LLC
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,9 @@
 
 package org.symphonyoss.integration.webhook.github;
 
-import static org.symphonyoss.integration.webhook.github.GithubEventConstants.GITHUB_HEADER_EVENT_NAME;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.symphonyoss.integration.json.JsonUtils;
 import org.symphonyoss.integration.model.message.Message;
 import org.symphonyoss.integration.webhook.WebHookIntegration;
@@ -27,77 +28,86 @@ import org.symphonyoss.integration.webhook.github.parser.DefaultGithubParser;
 import org.symphonyoss.integration.webhook.github.parser.GithubParser;
 import org.symphonyoss.integration.webhook.github.parser.GithubParserException;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
+import javax.annotation.PostConstruct;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
+import static org.symphonyoss.integration.webhook.github.GithubEventConstants.GITHUB_HEADER_EVENT_NAME;
 
 /**
  * Implementation of a WebHook to integrate with GITHUB, rendering it's messages.
- *
+ * <p>
  * Created by Milton Quilzini on 06/09/16.
  */
 @Component
 public class GithubWebHookIntegration extends WebHookIntegration {
 
-  private Map<String, GithubParser> parsers = new HashMap<>();
+    private Map<String, GithubParser> parsers = new HashMap<>();
 
-  @Autowired
-  private DefaultGithubParser defaultGithubParser;
+    @Autowired
+    private DefaultGithubParser defaultGithubParser;
 
-  @Autowired
-  private List<GithubParser> gitHubBeans;
+    @Autowired
+    private List<GithubParser> gitHubBeans;
 
-  @PostConstruct
-  public void init() {
-    // adds those with events to our parser map
-    for (GithubParser parser : gitHubBeans) {
-      List<String> events = parser.getEvents();
-      for (String eventType : events) {
-        this.parsers.put(eventType, parser);
-      }
-    }
-  }
-
-  @Override
-  public Message parse(WebHookPayload input) throws WebHookParseException {
-    try {
-      JsonNode rootNode = JsonUtils.readTree(input.getBody());
-      Map<String, String> parameters = input.getParameters();
-
-      String webHookEvent = input.getHeaders().get(GITHUB_HEADER_EVENT_NAME);
-
-      GithubParser parser = getParser(webHookEvent);
-
-      String formattedMessage = parser.parse(parameters, rootNode);
-
-      return super.buildMessageML(formattedMessage, webHookEvent);
-    } catch (IOException e) {
-      throw new GithubParserException("Something went wrong while trying to convert your message to the expected format", e);
-    }
-  }
-
-  /**
-   * Gets a GitHub parser based on the event.
-   * If none is found, we return a default parser.
-   * @param webHookEvent the webhook event being parsed.
-   * @return the most adequate parser to handle the webHookEvent.
-   */
-  private GithubParser getParser(String webHookEvent) {
-    GithubParser result = parsers.get(webHookEvent);
-
-    if (result == null) {
-      return defaultGithubParser;
+    @PostConstruct
+    public void init() {
+        // adds those with events to our parser map
+        for (GithubParser parser : gitHubBeans) {
+            List<String> events = parser.getEvents();
+            for (String eventType : events) {
+                this.parsers.put(eventType, parser);
+            }
+        }
     }
 
-    return result;
-  }
+    @Override
+    public Message parse(WebHookPayload input) throws WebHookParseException {
+        try {
+            JsonNode rootNode = JsonUtils.readTree(input.getBody());
+            Map<String, String> parameters = input.getParameters();
 
+            String webHookEvent = input.getHeaders().get(GITHUB_HEADER_EVENT_NAME);
+
+            GithubParser parser = getParser(webHookEvent);
+
+            String formattedMessage = parser.parse(parameters, rootNode);
+
+            return super.buildMessageML(formattedMessage, webHookEvent);
+        } catch (IOException e) {
+            throw new GithubParserException("Something went wrong while trying to convert your message to the expected format", e);
+        }
+    }
+
+    /**
+     * Gets a GitHub parser based on the event.
+     * If none is found, we return a default parser.
+     *
+     * @param webHookEvent the webhook event being parsed.
+     * @return the most adequate parser to handle the webHookEvent.
+     */
+    private GithubParser getParser(String webHookEvent) {
+        GithubParser result = parsers.get(webHookEvent);
+
+        if (result == null) {
+            return defaultGithubParser;
+        }
+
+        return result;
+    }
+
+    /**
+     * @see WebHookIntegration#getSupportedContentTypes()
+     */
+    @Override
+    public List<MediaType> getSupportedContentTypes() {
+        List<MediaType> supportedContentTypes = new ArrayList<>();
+        supportedContentTypes.add(MediaType.APPLICATION_JSON_TYPE);
+        return supportedContentTypes;
+    }
 }
 
